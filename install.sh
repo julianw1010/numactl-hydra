2a0aaba8f2b2a88632d89b8dc156c87264608f75
Julian W <j.w10@freenet.de>
2026-03-24 20:46:20 +0100

    Updated prefix

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 sudo apt-get update
 sudo apt-get install -y build-essential autoconf automake libtool pkg-config
 ./autogen.sh
-./configure
+./configure --prefix=/opt/numactl-hydra
 make -j$(nproc)
 sudo make install
 ldconfig
+sudo ln -s /opt/numactl-hydra/bin/numactl /usr/local/bin/numactl-hydra
