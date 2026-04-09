c6f75d634c96af0b825bb4223bb62f3896ecc46f
Julian W <julian.waciewski@rwth-aachen.de>
2026-04-09 08:31:12 +0000

    Updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,29 @@
-sudo apt-get update
-sudo apt-get install -y build-essential autoconf automake libtool pkg-config
+#!/usr/bin/env bash
+set -e
+
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    ID_LIKE_ALL="$ID $ID_LIKE"
+else
+    echo "Cannot detect OS"; exit 1
+fi
+
+case "$ID_LIKE_ALL" in
+    *debian*|*ubuntu*)
+        sudo apt-get update
+        sudo apt-get install -y build-essential autoconf automake libtool pkg-config
+        ;;
+    *fedora*|*rhel*|*centos*)
+        sudo dnf install -y @development-tools autoconf automake libtool pkgconfig gcc make
+        ;;
+    *)
+        echo "Unsupported distro: $ID"; exit 1
+        ;;
+esac
+
 ./autogen.sh
 ./configure --prefix=/opt/numactl-hydra
-make -j$(nproc)
+make -j"$(nproc)"
 sudo make install
-ldconfig
-sudo ln -s /opt/numactl-hydra/bin/numactl /usr/local/bin/numactl-hydra
+sudo ldconfig
+sudo ln -sf /opt/numactl-hydra/bin/numactl /usr/local/bin/numactl-hydra
